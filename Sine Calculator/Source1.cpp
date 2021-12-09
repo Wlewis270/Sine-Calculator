eaad92507b03f70800597f35bf385777eb481237
Wlewis270 <85946758+Wlewis270@users.noreply.github.com>
2021-12-09 10:15:11 +0000

    Pog
    Mostly done, need to do sine research

--- Sine Calculator/Source1.cpp
+++ Sine Calculator/Source1.cpp
@@ -1,1 +1,38 @@
-#include math.h
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+float num = 0, err = 0;
+
+
+void main() {
+	
+	cout << "Please enter a number you would like to test: ";
+
+	cin >> num;
+
+	cout << "\n\nPlease enter the allowable error: ";
+	
+	cin >> err;
+
+	int sign = 1, k = 1, iterations = 0;
+	float factorial = 6, xn1 = num, pwr = 2, n = 0, xn = 0;
+
+
+	do {
+		xn = xn1;
+		sign = sign * -1;
+		pwr = 2 * k + 1;
+		xn1 = xn + sign * (pow(num,pwr)) / factorial;
+		factorial = factorial * (pwr + 1) * (pwr + 2);
+		k = k + 1;
+		n = n + 1;
+		iterations += 1;
+
+	} while (abs(xn - xn1) > err);
+
+	cout << xn1 << "\n\nSine was calculated in " << iterations << " iterations\n\n";
+
+	system("pause");
+
+}
